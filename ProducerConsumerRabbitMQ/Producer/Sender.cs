a6163917c45fcb2035a2b1c76be5e9994d6fae77
davood.hanifi <davood.hanifi@gmail.com>
2022-02-02 15:44:36 +0330

    Added Producer.

---
+++ ProducerConsumerRabbitMQ/Producer/Sender.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace Producer
+{
+    public class Sender
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                    var message = "Getting started with .NET 6 RabbitMQ.";
+                    var body = System.Text.Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish("", "BasicTest", null, body);
+
+                    Console.WriteLine($"The message '{message}' has been sent.");
+                }
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
